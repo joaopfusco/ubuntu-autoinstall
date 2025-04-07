4932ce635467e53e227b9b9ff728a6a4bf899913
joaopfusco <125307931+joaopfusco@users.noreply.github.com>
2025-04-07 17:31:11 -0300

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -3,27 +3,29 @@
 set -e
 
 # Atualiza repositórios
-sudo apt-get update
+apt-get update
 
 # Instala dependências básicas
-sudo apt-get install -y software-properties-common dirmngr gpg lsb-release unzip
+apt-get install -y software-properties-common dirmngr gpg lsb-release unzip
 
 # Instalando o Google Chrome
 echo "Instalando Google Chrome..."
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+dpkg -i ./google-chrome-stable_current_amd64.deb
+apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
 
 # Instalando Docker
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker joaop
