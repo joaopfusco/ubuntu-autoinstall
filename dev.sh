d7c6036f973e571769014f69ae131f885302e497
joaopfusco <125307931+joaopfusco@users.noreply.github.com>
2025-04-07 18:09:50 -0300

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -14,18 +14,3 @@ wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 dpkg -i ./google-chrome-stable_current_amd64.deb
 apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
-
-# Instalando Docker
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-usermod -aG docker joaop
